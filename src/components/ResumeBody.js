cc52ecf1861f44c4905de7ad5abe7af7d3f0c5d0
Ken Yamada <yamakenth@gmail.com>
2021-12-30 16:10:23 +0700

    Add WorkExp to body

--- src/components/ResumeBody.js
+++ src/components/ResumeBody.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PersonalInfo from './PersonalInfo';
 import '../styles/ResumeBody.css';
+import WorkExp from './WorkExp';
 
 class ResumeBody extends React.Component {
   render() {
     return (
       <div className='resume-body'>
         <PersonalInfo />
+        <WorkExp />
       </div>
     );
   }
