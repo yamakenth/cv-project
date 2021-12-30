f3b256015534e67318b5140bc7dd6405376cae60
Ken Yamada <yamakenth@gmail.com>
2021-12-30 17:53:13 +0700

    Create form skeleton for Personal Info

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -6,7 +6,39 @@ class EditForm extends React.Component {
     return (
       <div style={this.props.formStyle}>
         <form className='edit-form'>
-          <h2>This is a form</h2>
+          <h2>Personal Information</h2>
+            {/*  */}
+            <div>
+              <div class='fname-input-container'>
+                <label htmlFor="fname-input">First Name:</label>
+                <input type="text" id="fname-input" />
+              </div>
+              <div class='lname-input-container'>
+                <label htmlFor="lname-input">Last Name:</label>
+                <input type="text" id="lname-input" />
+              </div>
+              <div class='location-input-container'>
+                <label htmlFor="location-input">Location:</label>
+                <input type="text" id="location-input" />
+              </div>
+              <div class='email-input-container'>
+                <label htmlFor="email-input">Email:</label>
+                <input type="text" id="email-input" />
+              </div>
+              <div class='phone-number-input-container'>
+                <label htmlFor="phone-number-input">Phone Number:</label>
+                <input type="text" id="phone-number-input" />
+              </div>
+              <div class='link-input-container'>
+                <label htmlFor="link-input">Link:</label>
+                <input type="text" id="link-input" />
+              </div>
+              <div class='description-input-container'>
+                <label htmlFor="description-input">Description:</label>
+                <textarea id="description-input" rows='4' cols='50' />
+              </div>
+            </div>
+            {/*  */}
           <button type='submit'>Confirm</button>
         </form>
         <div id='overlay'></div>
