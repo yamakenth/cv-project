6a89e94b53df3571c7ebef57bbd68c54ad3256fe
Ken Yamada <yamakenth@gmail.com>
2021-12-30 16:34:27 +0700

    Remove whitespace for readability

--- src/components/EditButton.js
+++ src/components/EditButton.js
@@ -4,9 +4,7 @@ import '../styles/EditButton.css';
 class EditButton extends React.Component {
   render() {
     return (
-      <button className='edit-button' style={this.props.style}>
-        Edit
-      </button>
+      <button className='edit-button' style={this.props.style}>Edit</button>
     )
   }
 }
