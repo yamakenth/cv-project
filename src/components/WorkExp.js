0940412e29ffe3a98aa63376d85c60001658d808
Ken Yamada <yamakenth@gmail.com>
2021-12-30 15:15:26 +0700

    Add basic structure and style

---
+++ src/components/WorkExp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/WorkExp.css';
+
+class WorkExp extends React.Component {
+  render() {
+    return (
+      <div className='work-experience'>
+        <h3>WORK EXPERIENCE</h3>
+      </div>
+    );
+  }
+}
+
+export default WorkExp;
