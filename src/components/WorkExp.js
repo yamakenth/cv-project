5ff69484ddbcbb0ecccf5bb5e0f82e7ce91b7d0a
Ken Yamada <yamakenth@gmail.com>
2021-12-30 20:05:02 +0700

    Add data as state

--- src/components/WorkExp.js
+++ src/components/WorkExp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import uniqid from 'uniqid';
 import EditButton from './EditButton';
 
 class WorkExp extends React.Component {
@@ -7,6 +8,50 @@ class WorkExp extends React.Component {
     this.state = {
       style: {
         editBtn: { display: 'none' }
+      },
+      data: {
+        job: {
+          id: uniqid(),
+          company: '',
+          location: '',
+          position: '',
+          date: '',
+          desc_1: '',
+          desc_2: '',
+          desc_3: ''
+        },
+        jobs: [
+          {
+            id: uniqid(),
+            company: 'An E-commerce Company',
+            location: 'Bangkok, Thailand',
+            position: 'Data Analyst',
+            date: 'September 2020 - October 2021',
+            desc_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas tellus rutrum tellus pellentesque. Tincidunt vitae semper quis lectus nulla at volutpat.',
+            desc_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Donec et odio pellentesque diam volutpat commodo sed egestas egestas.',
+            desc_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis magna etiam tempor orci eu lobortis elementum nibh. Adipiscing elit ut aliquam purus sit amet luctus.'
+          },
+          {
+            id: uniqid(),
+            company: 'A Manufacturing Company',
+            location: 'Chonburi, Thailand',
+            position: 'Industrial Engineer',
+            date: 'September 2019 - October 2020',
+            desc_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas tellus rutrum tellus pellentesque. Tincidunt vitae semper quis lectus nulla at volutpat.',
+            desc_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Donec et odio pellentesque diam volutpat commodo sed egestas egestas.',
+            desc_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis magna etiam tempor orci eu lobortis elementum nibh. Adipiscing elit ut aliquam purus sit amet luctus.'
+          },
+          {
+            id: uniqid(),
+            company: 'A Banking Company',
+            location: 'Bangkok, Thailand',
+            position: 'Investment Banker',
+            date: 'September 2018 - October 2019',
+            desc_1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas tellus rutrum tellus pellentesque. Tincidunt vitae semper quis lectus nulla at volutpat.',
+            desc_2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Donec et odio pellentesque diam volutpat commodo sed egestas egestas.',
+            desc_3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis magna etiam tempor orci eu lobortis elementum nibh. Adipiscing elit ut aliquam purus sit amet luctus.'
+          }
+        ]
       }
     }
   }
@@ -36,69 +81,25 @@ class WorkExp extends React.Component {
       >
         <EditButton editBtnStyle={this.state.style.editBtn} />
         <h3>WORK EXPERIENCE</h3>
-        <div className='work one item'>
-          <div className='row one'>
-            <p>An E-commerce Company</p>
-            <p>Bangkok, Thailand</p>
-          </div>
-          <div className='row two'>
-            <p>Data Analyst</p>
-            <p>September 2020 - October 2021</p>
-          </div>
-          <ul className='job-description'>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas tellus rutrum tellus pellentesque. Tincidunt vitae semper quis lectus nulla at volutpat.
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Donec et odio pellentesque diam volutpat commodo sed egestas egestas.
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis magna etiam tempor orci eu lobortis elementum nibh. Adipiscing elit ut aliquam purus sit amet luctus.
-            </li>
-          </ul>
-        </div>
-        <div className='work two item'>
-          <div className='row one'>
-            <p>A Manufacturing Company</p>
-            <p>Chonburi, Thailand</p>
-          </div>
-          <div className='row two'>
-            <p>Industrial Engineer</p>
-            <p>September 2019 - October 2020</p>
-          </div>
-          <ul className='job-description'>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas tellus rutrum tellus pellentesque. Tincidunt vitae semper quis lectus nulla at volutpat.
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Donec et odio pellentesque diam volutpat commodo sed egestas egestas.
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis magna etiam tempor orci eu lobortis elementum nibh. Adipiscing elit ut aliquam purus sit amet luctus.
-            </li>
-          </ul>
-        </div>
-        <div className='work three item'>
-          <div className='row one'>
-            <p>A Banking Company</p>
-            <p>Bangkok, Thailand</p>
-          </div>
-          <div className='row two'>
-            <p>Investment Banker</p>
-            <p>September 2018 - October 2019</p>
-          </div>
-          <ul className='job-description'>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas tellus rutrum tellus pellentesque. Tincidunt vitae semper quis lectus nulla at volutpat.
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Donec et odio pellentesque diam volutpat commodo sed egestas egestas.
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis magna etiam tempor orci eu lobortis elementum nibh. Adipiscing elit ut aliquam purus sit amet luctus.
-            </li>
-          </ul>
-        </div>
+        {this.state.data.jobs.map((job) => {
+          return (
+            <div key={job.id} className='work item'>
+              <div className='row one'>
+                <p>{job.company}</p>
+                <p>{job.location}</p>
+              </div>
+              <div className='row two'>
+                <p>{job.position}</p>
+                <p>{job.date}</p>
+              </div>
+              <ul className='job-description'>
+                <li>{job.desc_1}</li>
+                <li>{job.desc_2}</li>
+                <li>{job.desc_3}</li>
+              </ul>
+            </div>
+          );
+        })}
       </div>
     );
   }
