9b94b4dcc18f86cd53ac7ebac17c0976b5a5285d
Ken Yamada <yamakenth@gmail.com>
2021-12-31 16:40:25 +0700

    Add functionality to delete button

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -5,7 +5,7 @@ import closeButton from '../img/close.png';
 export default class DeleteButton extends React.Component {
   render() {
     return (
-      <button className='delete-button'>
+      <button className='delete-button' onClick={this.props.handleDelete}>
         <img src={closeButton} alt=''/>
       </button>
     )
