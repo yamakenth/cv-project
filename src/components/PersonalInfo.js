fde8f0825acebd59082db67010927605c642b82b
Ken Yamada <yamakenth@gmail.com>
2022-01-09 22:28:39 +0700

    Create custom hook to handle input form

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -19,30 +19,16 @@ export default function PersonalInfo() {
     setDisplayFormStyle({ display: 'block' });
   }
 
-  const [fname, setFName] = useState('James');
-  const handleFNameChange = (e) => setFName(e.target.value);
-  
-  const [lname, setLName] = useState('Jones');
-  const handleLNameChange = (e) => setLName(e.target.value);
-  
-  const [location, setLocation] = useState('Seattle, Washington');
-  const handleLocationChange = (e) => setLocation(e.target.value);
-  
-  const [email, setEmail] = useState('contactme@gmail.com');
-  const handleEmailChange = (e) => setEmail(e.target.value);
-  
-  const [phoneNumber, setPhoneNumber] = useState('(123)-567-8910');
-  const handlePhoneNumberChange = (e) => setPhoneNumber(e.target.value);
-
-  const [linkHref, setLinkHref] = useState(
+  const fname = useFormInput('James');
+  const lname = useFormInput('Jones');
+  const location = useFormInput('Seattle, Washington');
+  const email = useFormInput('contactme@gmail.com');
+  const phoneNumber = useFormInput('(123)-567-8910');
+  const linkHref = useFormInput(
     'https://www.youtube.com/watch?v=UqTTSo1wdIY&ab_channel=F1AudioBook'
   );
-  const handleLinkHrefChange = (e) => setLinkHref(e.target.value);
-
-  const [linkText, setLinkText] = useState('www.myportfolio.com');
-  const handleLinkTextChange = (e) => setLinkText(e.target.value);
-
-  const [description, setDescription] = useState(
+  const linkText = useFormInput('www.myportfolio.com');
+  const description = useFormInput(
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ' +
     'ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ' + 
     'ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in ' + 
@@ -50,21 +36,20 @@ export default function PersonalInfo() {
     'sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est ' +
     'laborum.'
   );
-  const handleDescriptionChange = (e) => setDescription(e.target.value);
 
   return (
     <div className='personal-info section'>
       <PersonalInfoDisplay 
         editBtnStyle={editBtnStyle}
         displayFormStyle={displayFormStyle}
-        fname={fname}
-        lname={lname}
-        location={location}
-        email={email}
-        phoneNumber={phoneNumber}
-        linkHref={linkHref}
-        linkText={linkText}
-        description={description}
+        fname={fname.value}
+        lname={lname.value}
+        location={location.value}
+        email={email.value}
+        phoneNumber={phoneNumber.value}
+        linkHref={linkHref.value}
+        linkText={linkText.value}
+        description={description.value}
 
         showEditBtn={showEditBtn}
         hideEditBtn={hideEditBtn}
@@ -72,25 +57,38 @@ export default function PersonalInfo() {
       />
       <PersonalInfoEdit 
         editFormStyle={editFormStyle}
-        fname={fname}
-        lname={lname}
-        location={location}
-        email={email}
-        phoneNumber={phoneNumber}
-        linkHref={linkHref}
-        linkText={linkText}
-        description={description}
+        fname={fname.value}
+        lname={lname.value}
+        location={location.value}
+        email={email.value}
+        phoneNumber={phoneNumber.value}
+        linkHref={linkHref.value}
+        linkText={linkText.value}
+        description={description.value}
         
         hideEditForm={hideEditForm}
-        handleFNameChange={handleFNameChange}
-        handleLNameChange={handleLNameChange}
-        handleLocationChange={handleLocationChange}
-        handleEmailChange={handleEmailChange}
-        handlePhoneNumberChange={handlePhoneNumberChange}
-        handleLinkTextChange={handleLinkTextChange}
-        handleLinkHrefChange={handleLinkHrefChange}
-        handleDescriptionChange={handleDescriptionChange}
+        handleFNameChange={fname.onChange}
+        handleLNameChange={lname.onChange}
+        handleLocationChange={location.onChange}
+        handleEmailChange={email.onChange}
+        handlePhoneNumberChange={phoneNumber.onChange}
+        handleLinkTextChange={linkText.onChange}
+        handleLinkHrefChange={linkHref.onChange}
+        handleDescriptionChange={description.onChange}
       />
     </div>
   )
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
+  }
 }
