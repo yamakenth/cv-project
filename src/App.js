98fefd7e94f79af0de2aad0b995c31d74f166d2e
Ken Yamada <yamakenth@gmail.com>
2022-01-09 21:10:13 +0700

    Convert to functional component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import PersonalInfo from './components/PersonalInfo';
 import WorkExp from './components/WorkExp';
 import './styles/App.css';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className='resume-body'>
-        <PersonalInfo />
-        <WorkExp />
-        <Education />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className='resume-body'>
+      <PersonalInfo />
+      <WorkExp />
+      <Education />
+    </div>
+  )
 }
